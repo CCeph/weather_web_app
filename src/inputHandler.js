99f3c354b5a0221c72d5522f9c6acd18e11dbd60
CCeph <yousefelsisyy@gmail.com>
2023-06-21 13:16:08 -0300

    Prevent autocomplete when input is empty

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -16,6 +16,11 @@ cachedDOM.$locationForm.addEventListener("click", (e) => {
 
 async function getCitySuggestions(searchString) {
   try {
+    // Avoids calling the API if the input is empty to avoid error 400.
+    if (searchString === "") {
+      return "Empty search string";
+    }
+
     const response = await fetch(
       `https://api.locationiq.com/v1/autocomplete?key=pk.9c87a186a6a34695dbff71ab9dd7f6bc&q=${searchString}&tag=place:city`,
       { mode: "cors" }
@@ -23,17 +28,24 @@ async function getCitySuggestions(searchString) {
     const suggestions = await response.json();
     return suggestions;
   } catch (error) {
-    return console.log("Custom", error);
+    return console.log(error);
   }
 }
 
 async function filterForAddress(suggestionsPromise) {
   try {
     const suggestions = await suggestionsPromise;
+    const filteredSuggestions = [];
+
+    // Handles the case of an empty input value.
+    if (suggestions === "Empty search string") {
+      return filteredSuggestions;
+    }
+
     if (suggestions.error === "Unable to geocode") {
       throw new Error("Invalid city name");
     }
-    const filteredSuggestions = [];
+
     suggestions.forEach((element) => {
       filteredSuggestions.push(element.display_address);
     });
@@ -43,7 +55,7 @@ async function filterForAddress(suggestionsPromise) {
   }
 }
 
-cachedDOM.$location.addEventListener("keydown", () => {
+cachedDOM.$location.addEventListener("input", () => {
   const citySuggestions = getCitySuggestions(cachedDOM.$location.value);
   const filteredSuggestions = filterForAddress(citySuggestions);
   console.log(filteredSuggestions);
