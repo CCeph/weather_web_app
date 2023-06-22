90793b544ef087cded042f49a3a5cae6349e245d
CCeph <yousefelsisyy@gmail.com>
2023-06-22 15:06:28 -0300

    Enable autofilling using autocomplete

--- src/outputHandler.js
+++ src/outputHandler.js
@@ -4,21 +4,29 @@ import { pubsubEventNames } from "./eventsHandler";
 // Cache DOM Elements
 function createDOMCache() {
   const $autocomplete = document.querySelector("[data-autocomplete]");
+  const $location = document.getElementById("location");
   return {
     $autocomplete,
+    $location,
   };
 }
 
 const cachedDOM = createDOMCache();
 
+function autoFillLocationInput() {
+  cachedDOM.$location.value = this.textContent;
+  cachedDOM.$autocomplete.textContent = "";
+}
+
 async function outputAutocomplete(eventMsg, suggestionsPromise) {
   const container = cachedDOM.$autocomplete;
-  container.innerHTML = "";
+  container.textContent = "";
 
   const suggestions = await suggestionsPromise;
   suggestions.forEach((suggestion) => {
     const div = document.createElement("div");
     div.textContent = suggestion;
+    div.addEventListener("click", autoFillLocationInput);
     container.appendChild(div);
   });
 }
