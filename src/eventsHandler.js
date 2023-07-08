a12699cec15af1eb17fe584ae16d8f18b91aef40
cceph <yousefelsisyy@gmail.com>
2023-07-08 12:54:42 -0300

    Fix bug where hidden autocomplete can be pressed

--- src/eventsHandler.js
+++ src/eventsHandler.js
@@ -17,3 +17,34 @@ function createPubsubEventNames() {
 
 // eslint-disable-next-line import/prefer-default-export
 export const pubsubEventNames = createPubsubEventNames();
+
+function createDOMCache() {
+  const $location = document.getElementById("location");
+  const $autocomplete = document.querySelector("[data-autocomplete]");
+  const $secondLocation = document.querySelector("[data-secondLocation]");
+  const $secondAutocomplete = document.querySelector(
+    "[data-secondAutocomplete]"
+  );
+  return {
+    $location,
+    $autocomplete,
+    $secondLocation,
+    $secondAutocomplete,
+  };
+}
+
+const cachedDOM = createDOMCache();
+
+function disableAutocomplete() {
+  if (!(document.activeElement === cachedDOM.location)) {
+    cachedDOM.$autocomplete.style.pointerEvents = "none";
+  }
+}
+
+cachedDOM.$location.addEventListener("blur", () => {
+  setTimeout(disableAutocomplete, 500);
+});
+
+cachedDOM.$location.addEventListener("focus", () => {
+  cachedDOM.$autocomplete.style.pointerEvents = "auto";
+});
